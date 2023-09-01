8ac2b25243a1da6f9d865fc1a1284c1ac5dd17a3
Thiago Fontes <13390191+ThiagoFontes@users.noreply.github.com>
2023-09-01 00:21:36 -0300

    chore(LBI_V1): lowering dart sdk requirements

--- logarithm_big_int/test/logarithm_big_int_test.dart
+++ logarithm_big_int/test/logarithm_big_int_test.dart
@@ -1,43 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:logarithm_big_int/logarithm_big_int.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
     test('Calculate logarithms', () {
-      List<({BigInt value, BigInt base, double result})> values = [
-        (
+      List<_TestValues> values = [
+        _TestValues(
           value: 10.toBigInt,
-          base: 10.toBigInt,
+          baseValue: 10.toBigInt,
           result: 1,
         ),
-        (
-          value: 1133.toBigInt,
-          base: 2.toBigInt,
-          result: 10.145932145820515637260235860156
-        ),
-        (
+        _TestValues(
+            value: 1133.toBigInt,
+            baseValue: 2.toBigInt,
+            result: 10.145932145820515637260235860156),
+        _TestValues(
           value: 8455742254424.toBigInt,
-          base: 8.toBigInt,
+          baseValue: 8.toBigInt,
           result: 14.314356180070492568714359515064,
         ),
-        (
+        _TestValues(
           value: 1.toBigInt,
-          base: 5.toBigInt,
+          baseValue: 5.toBigInt,
           result: 0,
         ),
-        (
+        _TestValues(
           value: 1.toBigInt,
-          base: 10.toBigInt,
+          baseValue: 10.toBigInt,
           result: 0,
         ),
-        (
+        _TestValues(
           value: 1588889988888.toBigInt,
-          base: 12.toBigInt,
+          baseValue: 12.toBigInt,
           result: 11.305880150693760157433025486197,
         ),
-        (
+        _TestValues(
           value: 1755526554455655528.toBigInt,
-          base: 64.toBigInt,
+          baseValue: 64.toBigInt,
           result: 10.101101587809681799628006458237,
         ),
       ];
@@ -45,13 +45,25 @@ void main() {
       for (final entry in values) {
         final staticValue = LogarithmBigInt.bigLog(
           value: entry.value,
-          logBase: entry.base,
+          logBase: entry.baseValue,
         );
-        final extensionValue = entry.value.bigLog(entry.base);
+        final extensionValue = entry.value.bigLog(entry.baseValue);
 
         expect(staticValue, extensionValue);
         expect(extensionValue, entry.result.toBigInt);
       }
     });
+  });
+}
+
+class _TestValues {
+  final BigInt value;
+  final BigInt baseValue;
+  final double result;
+
+  _TestValues({
+    required this.value,
+    required this.baseValue,
+    required this.result,
   });
 }
